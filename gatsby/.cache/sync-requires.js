fff706efea5929c73fe7ddf98c3ae7a1fde07f75
Michael Botes <michaelbotes21@gmail.com>
2021-01-13 22:23:57 +0200

    Checkpoint 8 Typography

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Michael Botes\\Documents\\Code\\personal\\master-gatsby\\gatsby\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Michael Botes\\Documents\\Code\\personal\\master-gatsby\\gatsby\\src\\pages\\404.js"))),
-  "component---src-pages-beers-js": hot(preferDefault(require("C:\\Users\\Michael Botes\\Documents\\Code\\personal\\master-gatsby\\gatsby\\src\\pages\\beers.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Michael Botes\\Documents\\Code\\personal\\master-gatsby\\gatsby\\src\\pages\\index.js"))),
-  "component---src-pages-pizzas-js": hot(preferDefault(require("C:\\Users\\Michael Botes\\Documents\\Code\\personal\\master-gatsby\\gatsby\\src\\pages\\pizzas.js"))),
-  "component---src-pages-slicemasters-js": hot(preferDefault(require("C:\\Users\\Michael Botes\\Documents\\Code\\personal\\master-gatsby\\gatsby\\src\\pages\\slicemasters.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/michaelbotes/Documents/Code.nosync/Personal/master-gatsby-course/gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/michaelbotes/Documents/Code.nosync/Personal/master-gatsby-course/gatsby/src/pages/404.js"))),
+  "component---src-pages-beers-js": hot(preferDefault(require("/Users/michaelbotes/Documents/Code.nosync/Personal/master-gatsby-course/gatsby/src/pages/beers.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/michaelbotes/Documents/Code.nosync/Personal/master-gatsby-course/gatsby/src/pages/index.js"))),
+  "component---src-pages-pizzas-js": hot(preferDefault(require("/Users/michaelbotes/Documents/Code.nosync/Personal/master-gatsby-course/gatsby/src/pages/pizzas.js"))),
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/michaelbotes/Documents/Code.nosync/Personal/master-gatsby-course/gatsby/src/pages/slicemasters.js")))
 }
 
